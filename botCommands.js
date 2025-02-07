fda921a6ed44e1b678ac42bced0229f23acd7fdb
Aritra <aritra.2003k@gmail.com>
2025-02-07 14:43:58 +0530

    Fixed bugs

--- botCommands.js
+++ botCommands.js
@@ -24,7 +24,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
     try {
         console.log('Started refreshing application (/) commands.');
     
-        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] });
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
         // await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID , '1333352234620293131'), { body: commands });
     
         console.log('Successfully reloaded application (/) commands.');
